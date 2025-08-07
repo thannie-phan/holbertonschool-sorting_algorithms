4f4fe91fad34e07dcee830323e96b78b06cdce06
“thannie-phan” <thannie.phan@gmail.com>
2025-08-07 11:22:57 +0000

    task0

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -22,18 +22,17 @@ void bubble_sort(int *array, size_t size)
 	while (total_pass < size - 1)
 		{
 		actual_pass = 0;
-		while (actual_pass < size - total_pass - 1)
+			while (actual_pass < size - total_pass - 1)
 			{
-			if (array[actual_pass] > array[actual_pass + 1])
-			{
-			temp = array[actual_pass];
-			array[actual_pass] = array[actual_pass + 1];
-			array[actual_pass + 1] = temp;
-			print_array(array, size);
+				if (array[actual_pass] > array[actual_pass + 1])
+				{
+					temp = array[actual_pass];
+					array[actual_pass] = array[actual_pass + 1];
+					array[actual_pass + 1] = temp;
+					print_array(array, size);
+				}
+				actual_pass++;
 			}
-			actual_pass++;
-			}
-		total_pass++;
 		}
 }
 
